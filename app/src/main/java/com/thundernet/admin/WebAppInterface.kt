c780e8d0ae8e0b45f0b066717a8864e1c2152eb5
ThunderCoreDev <castellashly0@gmail.com>
2026-01-05 01:46:11 -0500

    Last Sync: 2026-01-05 01:46 (Mobile)

--- app/src/main/java/com/thundernet/admin/WebAppInterface.kt
+++ app/src/main/java/com/thundernet/admin/WebAppInterface.kt
@@ -3,7 +3,6 @@ package com.thundernet.admin
 import android.content.Context
 import android.content.Intent
 import com.thundernet.web.WebActivity
-import android.content.Intent
 import android.content.SharedPreferences
 import android.webkit.JavascriptInterface
 import android.widget.Toast
--- app/src/main/java/com/thundernet/web/WebActivity.kt
+++ app/src/main/java/com/thundernet/web/WebActivity.kt
@@ -1,6 +1,7 @@
 package com.thundernet.web
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
