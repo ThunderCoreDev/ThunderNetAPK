85bf387a7075d188f1364c7094fe10eda17dd63d
ThunderCoreDev <castellashly0@gmail.com>
2026-01-05 01:36:21 -0500

    Last Sync: 2026-01-05 01:36 (Mobile)

--- app/src/main/java/com/thundernet/admin/WebAppInterface.kt
+++ app/src/main/java/com/thundernet/admin/WebAppInterface.kt
@@ -1,6 +1,7 @@
 package com.thundernet.admin
 
 import android.content.Context
+import android.content.Intent
 import com.thundernet.web.WebActivity
 import android.content.Intent
 import android.content.SharedPreferences
